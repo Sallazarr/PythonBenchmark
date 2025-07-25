5a6bd12b7c901ff6621a8a400c355499acce629c
Henrique Salazar da Silva <henriquesalazards16@hotmail.com>
2025-07-25 14:00:33 -0300

    Correção no exe do Electron

--- SalazarBenchmarkElectron/index.js
+++ SalazarBenchmarkElectron/index.js
@@ -4,9 +4,31 @@ const benchmark = require("./NodeBenchmark/nodeBenchmark")
 const url = require("url");
 
 const {connectDb, getInstanceDb, closeConnectionDb} = require('./database/connection');
+const fs = require('fs');
+
+const logPath = path.join(app.getPath('userData'), 'app.log');
+const logStream = fs.createWriteStream(logPath, { flags: 'a' });
+
+function logToFile(...args) {
+  const message = args.map(arg => (typeof arg === 'string' ? arg : JSON.stringify(arg))).join(' ') + '\n';
+  logStream.write(message);
+}
+
+console.log = (...args) => {
+  logToFile(...args);
+  process.stdout.write(args.join(' ') + '\n'); // ainda manda pro stdout se possível
+};
+console.error = (...args) => {
+  logToFile(...args);
+  process.stderr.write(args.join(' ') + '\n');
+};
+
+console.log('=== App started ===');
+
 
 // Cria a janela
 function createWindow() {
+  console.log("Criando janela...")
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -18,14 +40,21 @@ function createWindow() {
     },
   });
 
-  const startUrl =
-    process.env.ELECTRON_START_URL ||
-    url.format({
-      pathname: path.join(__dirname, "./renderer/dist/index.html"),
-      protocol: "file:",
-      slashes: true,
-    });
+let startUrl;
 
+if (process.env.ELECTRON_START_URL) {
+  console.log("🚧 Modo DEV detectado.");
+  startUrl = process.env.ELECTRON_START_URL;
+} else {
+  console.log("✅ Modo produção detectado.");
+  startUrl = url.format({
+    pathname: path.join(__dirname, "renderer", "dist", "index.html"),
+    protocol: "file:",
+    slashes: true,
+  });
+}
+
+    console.log("start URL:", startUrl)
   win.loadURL(startUrl);
   //win.removeMenu(); 
   win.webContents.openDevTools(); /* pra abrir o devtools no executavel */
@@ -45,16 +74,19 @@ ipcMain.handle("rodar-benchmark", async () => {
 
 // Inicializa o app
 app.whenReady().then(async () => {
-  await connectDb(); // chama conexão com mongo
-  createWindow();
+//   console.log("💡 App ready, conectando ao Mongo...");
+//  // await connectDb(); // chama conexão com mongo
+ createWindow();
 
   app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+    if (BrowserWindow.getAllWindows().length === 0){
+      console.log("Ativando App") 
+      createWindow();
+}});
+ });
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
 
-app.on("will-quit", closeConnectionDb) //mata conexão com mongo
+// app.on("will-quit", closeConnectionDb) //mata conexão com mongo
